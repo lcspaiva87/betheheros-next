3c01c690c9a66c8327f329280f975028f2e22660
lucas paiva <lcaspaiva87@gmail.com>
2023-02-05 20:12:56 -0300

    fix:create method post ongs

--- src/pages/api/ongs/index.ts
+++ src/pages/api/ongs/index.ts
@@ -1,23 +1,40 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { PrismaClient } from "@prisma/client";
+import { ongs } from "@prisma/client";
 import { getOngs, postOngs } from "../../../lib";
 import { ongsProps } from "@/@types/ongs";
 
 export default async function ongs(req: NextApiRequest, res: NextApiResponse) {
-  const { method } = req;
-  const prisma = new PrismaClient();
-  if (method === "GET") {
-    const ongs = getOngs();
-    return res.status(200).json({
-      data: ongs,
-    });
-  } else if (method === "POST") {
-    const { name, uf, city, whatsapp, email }: ongsProps = req.body;
-    const ongs = postOngs({name, uf, city, whatsapp, email});
-    return res.status(200).json({
-      data: ongs,
-    });
-  }
+  try {
+    const { method } = req;
 
-  return res.status(404).json({ message: "Router not found" });
+    if (method === "GET") {
+      const ongs = getOngs();
+      return res.status(200).json({
+        data: ongs,
+      });
+    } else if (method === "POST") {
+      const { name, uf, city, whatsapp, email }: ongsProps = req.body;
+      if(!name){
+        return res.status(404).json({ message: "Campo nome esta vazio" });
+      }
+      if(!uf){
+        return res.status(404).json({ message: "Campo uf esta vazio" });
+      }
+      if(!city){
+        return res.status(404).json({ message: "Campo cidade esta vazio" });
+      }
+      if(!whatsapp){
+        return res.status(404).json({ message: "Campo whatsapp esta vazio" });
+      }
+      if(!email){
+        return res.status(404).json({ message: "Campo email esta vazio" });
+      }
+      const ongs: Promise<ongs> = postOngs({ name, uf, city, whatsapp, email });
+      return res.status(200).json({
+        data: ongs,
+      });
+    }
+  } catch (error) {
+    return res.status(404).json({ message: error });
+  }
 }
