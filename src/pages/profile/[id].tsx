7e6bca0e2fc701904a79ab9edaa5f635bdbc5d99
lucas paiva <lcaspaiva87@gmail.com>
2023-02-13 20:44:50 -0300

    remove bug

--- src/pages/profile/[id].tsx
+++ src/pages/profile/[id].tsx
@@ -1,11 +1,5 @@
+import { ProfileView } from "../../views/profile";
 
-import { GetServerSideProps } from "next";
-import {ProfileView} from "../../views/profile";
-import { ongsProps } from '@/@types/ongs';
-import { getIncidents } from "@/services/incidents";
-import { useRouter } from "next/router";
-export default function profile(props:any) {
-  return <ProfileView   />;
+export default function profile() {
+  return <ProfileView />;
 }
-
-
